d1e0ef2f029ebee5c9cd2f790361646fa3f049ca
Sharkeee <sharkmen1996@gmail.com>
2018-02-06 20:24:23 +0100

    INterfaces

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
 public class Main {
+
+    public static void main (String[] args) {
+
+
+        ITelephone timsPhone;
+        timsPhone = new DeskPhone(123456);
+        timsPhone.powerOn();
+        timsPhone.callPhone(123456);
+        timsPhone.answer();
+
+        timsPhone = new MobilePhone(245656);
+         timsPhone.powerOn();
+        timsPhone.callPhone(245656);
+        timsPhone.answer();
+    }
+
 }
